e5f94e34293ca21df556138a23df4779388547cf
Sadan Mian <sadanmiyan007@gmail.com>
2023-08-02 22:52:30 +0530

    Share Component

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import "./feed.css";
+import Share from "../share/Share";
 
 export default function Feed() {
-  return <div className="feed">Feed</div>;
+  return (
+    <div className="feed">
+      <div className="feedWrapper">
+        <Share />
+      </div>
+    </div>
+  );
 }
