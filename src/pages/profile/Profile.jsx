15a0ce9cac891606542eca33c5d6cd8f9d1f7b52
Sadan Mian <sadanmiyan007@gmail.com>
2023-08-20 21:50:19 +0530

    rightbar profile added

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -24,7 +24,7 @@ const Profile = () => {
           </div>
           <div className="profileRightBottom">
             <Feed />
-            {/* <Rightbar /> */}
+            <Rightbar profile />
           </div>
         </div>
       </div>
